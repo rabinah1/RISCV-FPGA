15af450266941db3cd77e22093b9d3229de4f374
Henry Räbinä <henry.rabina@gmail.com>
2024-10-09 23:31:35 +0300

    Add tests for program memory

--- src/program_memory.vhd
+++ src/program_memory.vhd
@@ -7,6 +7,7 @@ entity program_memory is
     port (
         clk             : in    std_logic;
         reset           : in    std_logic;
+        test_reset      : in    std_logic;
         pc_in           : in    std_logic_vector(31 downto 0);
         instruction_reg : out   std_logic_vector(31 downto 0)
     );
@@ -25,6 +26,14 @@ begin
 
         if (reset = '1') then
             instruction_reg <= (others => '0');
+            prog_mem <= (others => (others => '0'));
+        elsif (test_reset = '1') then
+            instruction_reg <= (others => '0');
+            prog_mem(0) <= std_logic_vector(to_unsigned(2, 32));
+            prog_mem(1) <= std_logic_vector(to_unsigned(4, 32));
+            prog_mem(2) <= std_logic_vector(to_unsigned(6, 32));
+            prog_mem(3) <= std_logic_vector(to_unsigned(8, 32));
+            prog_mem(4) <= std_logic_vector(to_unsigned(10, 32));
         elsif (rising_edge(clk)) then
             instruction_reg <= prog_mem(to_integer(unsigned(pc_in)));
         end if;
