339cc8f19ef3db597183aabae468e028bd675b43
Henry Räbinä <henry.rabina@gmail.com>
2026-02-21 18:16:02 +0200

    Refactor code

--- src/test.c
+++ src/test.c
@@ -1,17 +1,17 @@
 #include <stdint.h>
 
 struct my_struct {
-    uint32_t member_1;
-    uint32_t member_2;
-    uint32_t member_3;
-    uint32_t member_4;
+    int32_t member_1;
+    int32_t member_2;
+    int32_t member_3;
+    int32_t member_4;
 };
 
-uint32_t test_function(uint32_t c, uint32_t res)
+int32_t test_function(int32_t c, int32_t res)
 {
-    uint32_t x = 5;
-    uint32_t y = 100;
-    uint32_t ret = 0;
+    int32_t x = 5;
+    int32_t y = 100;
+    int32_t ret = 0;
 
     if (res > c)
         ret = y - x + res;
@@ -21,7 +21,7 @@ uint32_t test_function(uint32_t c, uint32_t res)
     return ret;
 }
 
-uint32_t test_recursion(uint32_t k)
+int32_t test_recursion(int32_t k)
 {
     if (k > 0)
         return k + test_recursion(k - 1);
@@ -29,14 +29,26 @@ uint32_t test_recursion(uint32_t k)
         return 0;
 }
 
-uint32_t main(void)
+void modify_array(int32_t *arr)
 {
-    uint32_t a = 12;
-    uint32_t b = 5;
-    uint32_t c = 56;
-    uint32_t d = 30;
-    uint32_t res = 0;
-    uint32_t idx = 0;
+    arr[2] = 100;
+    arr[4] = -20;
+}
+
+int32_t main(void)
+{
+    int32_t a = 12;
+    int32_t b = 5;
+    int32_t c = 56;
+    int32_t d = 30;
+    int32_t res = 0;
+    int32_t idx = 0;
+    int32_t item_1 = 1;
+    int32_t item_2 = 2;
+    int32_t item_3 = 3;
+    int32_t item_4 = 4;
+    int32_t item_5 = 5;
+    int32_t arr[] = {item_1, item_2, item_3, item_4, item_5};
     struct my_struct s;
     s.member_1 = 5;
     s.member_2 = 10;
@@ -46,7 +58,7 @@ uint32_t main(void)
     if (a >= b) {
         res = a - b + test_recursion(10) - s.member_1;
 
-        for (uint32_t i  = 0; i < 10; i++)
+        for (int32_t i  = 0; i < 10; i++)
             res = res + i;
     } else {
         res = a + b - s.member_2;
@@ -62,6 +74,11 @@ uint32_t main(void)
             idx = idx + 3;
         }
     }
+
+    modify_array(arr);
+
+    for (int32_t i = 0; i < 5; i++)
+        res = res + arr[i];
 
     return res;
 }
